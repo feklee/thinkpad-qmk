630f9c2e4a91e9c740ac05caa8a3f71612dce9c8
Cong <rampadc@users.noreply.github.com>
2019-02-28 07:33:42 +1100

    Cloned from chibios_test example and adding ps/2

---
+++ keyboards/thinkpad/rules.mk
@@ -0,0 +1,13 @@
+#BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes	# Mouse keys
+EXTRAKEY_ENABLE = yes	# Audio control and System control
+CONSOLE_ENABLE = yes	# Console for debug
+COMMAND_ENABLE = yes    # Commands for debug and configuration
+SLEEP_LED_ENABLE = yes  # Breathing sleep LED during USB suspend
+NKRO_ENABLE = yes	    # USB Nkey Rollover
+CUSTOM_MATRIX = yes # Custom matrix file
+
+PS2_MOUSE_ENABLE = yes
+PS2_USE_INT = yes
+
+DEFAULT_FOLDER = thinkpad/trackpoint_twokey
